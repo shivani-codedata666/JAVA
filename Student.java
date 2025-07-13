8aa1656f3004d80778c09545b6b41eca9902e55d
shivani-codedata666 <shivanichavan129@gmail.com>
2025-07-13 20:55:55 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Student {
+    String name;
+    String address;
+    int[] marks = new int[5];
+    int total;
+    int average;
+    String grade;
+
+
+    void calculateGrade() {
+        total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        average = total / 5;
+
+        if (average >= 90) {
+            grade = "A";
+        } else if (average >= 80) {
+            grade = "B";
+        } else if (average >= 70) {
+            grade = "C";
+        } else if (average >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+
+        }
+    }
+
+    void display() {
+        System.out.println("name:" + name);
+        System.out.println("address:" + address);
+        System.out.println("marks:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("subject" + (i + 1) + ":" + marks[i]);
+        }
+        System.out.println("total:" + total);
+        System.out.println("average:" + average);
+        System.out.println("Grade:" + grade);
+    }
+}
+
